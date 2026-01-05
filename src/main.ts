b6edff4d6bca4e9912bf8bf9dc612d1ae6d182b6
illiaPimonov <howlyguy@gmail.com>
2026-01-06 00:10:55 +0100

    adds unused func

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ import { handlerReadiness } from "./api/readiness.js";
 import { handlerNotesCreate, handlerNotesGet } from "./api/notes.js";
 import { handlerUsersCreate, handlerUsersGet } from "./api/users.js";
 
+function unused() {
+  // this function does nothing
+  // and is called nowhere
+}
 const __dirname = path.resolve();
 
 if (!config.api.port) {
